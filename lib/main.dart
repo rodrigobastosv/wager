c453051d870b74c4be1918c00777c4e1282463c6
Rodrigo Bastos Vasconcelos <rodrigo.vasconcelos@wendys.com>
2022-08-18 20:48:11 -0300

    adding favicon

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Serh Consultoria',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
